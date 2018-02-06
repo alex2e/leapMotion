e3afd06aaba8502cdc31f0fd0a1b5efa31b12e7d
DESKTOP-7U6IS1N\Alejandro <a.santillana@aol.com>
2018-02-06 09:40:48 +0100

    Creación Botones pulsables y con sonido

---
+++ LeapMotion/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    /// <summary>
+    /// Referencia al aocmponente AudioSource
+    /// </summary>
+    private AudioSource audiosource;
+
+	// Use this for initialization
+	void Start () {
+        //Recupermaos la referencia al componente audiosource
+        audiosource = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Llama al play del AudioSource
+    /// </summary>
+    public void click()
+    {
+        audiosource.Play();
+    }
+}
